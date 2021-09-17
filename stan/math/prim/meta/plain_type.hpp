784fdb0f45bf6882bddd989cb2816924cfc2912a
Stan Jenkins <mc.stanislaw@gmail.com>
2021-09-17 22:44:49 +0000

    [Jenkins] auto-formatting by clang-format version 6.0.0-1ubuntu2~16.04.1 (tags/RELEASE_600/final)

--- stan/math/prim/meta/plain_type.hpp
+++ stan/math/prim/meta/plain_type.hpp
@@ -62,19 +62,22 @@ struct has_eval<T, void_t<decltype(std::declval<std::decay_t<T>&>().eval())>>
 
 }  // namespace internal
 
-
 /**
  * Determines plain (non expression) type associated with \c T. For \c Eigen
  * expression it is a type the expression can be evaluated into.
  * @tparam T type to determine plain type of
  */
 template <typename T>
-struct plain_type<T, require_t<bool_constant<internal::has_eval<T>::value && is_eigen<T>::value>>> {
+struct plain_type<T, require_t<bool_constant<internal::has_eval<T>::value
+                                             && is_eigen<T>::value>>> {
   using type = std::decay_t<decltype(std::declval<T&>().eval())>;
 };
 
 template <typename T>
-struct plain_type<T, require_t<bool_constant<!internal::has_eval<T>::value && internal::has_plain_object<T>::value && is_eigen<T>::value>>> {
+struct plain_type<
+    T, require_t<bool_constant<!internal::has_eval<T>::value
+                               && internal::has_plain_object<T>::value
+                               && is_eigen<T>::value>>> {
   using type = typename std::decay_t<T>::PlainObject;
 };
 
